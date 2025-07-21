14f4c3fd7fb6f83bbcbccd891d817f94e6ca1d69
mithulcrafts <codinggraphicsdesiging@gmail.com>
2025-07-22 02:55:33 +0530

    After modifying footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,9 +1,34 @@
-
+import { useNavigate } from 'react-router-dom';
+import AppLogo from '../../assets/images/AppLogoTitleAndCaption3D.png';
+import '../../styles/Footer.css'
 export default function Footer(){
+    const navigate=useNavigate();
     return(
         <>
-            <div style={{height:"200px",width:"100%",backgroundColor:"black",color:"white"}}>
-                
+            <div style={{height:"1000px",width:"100%",backgroundColor:"black",color:"white",display:"flex",flexDirection:"column",border:"0",position:"relative"}}>
+                <img src={AppLogo} alt="App Logo with Caption" height='250px' width='250px'style={{position:"absolute",left:"50%",transform:'translateX(-50%)'}}></img>
+                <p id="Caption">Your trusted online marketplace offering premium products with exceptional quality and service. We're committed to making shopping convenient, secure, and enjoyable for everyone.</p>
+                <nav style={{display:'flex',flexDirection:'Column',transform:"translate(30px,200px)"}}>
+                    <div style={{height:"parent",display:'flex',flexDirection:'column',justifyContent:'center'}}>
+                        <h1>Quick Links</h1>
+                        <p>About</p>
+                        <p>Our story</p>
+                        <p>Home</p>
+                        <p>Login/SignUp</p>
+                        <p>Cart</p>
+                        <p>Wishlist</p>
+                    </div>
+                    <div style={{height:"parent",display:'flex',flexDirection:'column',aligncontent:'center',justifyContent:'center'}}>
+                        <h1>Customer Care</h1>
+                        <p>Help Center</p>
+                        <p>Size Guide</p>
+                    </div>
+                    <div style={{height:"parent",display:'flex',flexDirection:'column',aligncontent:'center',justifyContent:'center'}}>
+                        <h1>Contact Info</h1>
+                        <p>ecommercedevkriti@gmail.com</p>
+                    </div>
+                </nav>
+                <h3 id="Copyrights">© 2025 Scroll 2 Ship. All rights reserved.</h3>
             </div>
         </>
     )
